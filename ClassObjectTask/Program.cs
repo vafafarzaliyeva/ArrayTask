04467153d4ef9f12e62c0f4ef1042671505d9048
vafafarzaliyeva <vafahf@code.edu.az>
2022-02-17 02:23:39 +0300

    First Commit

---
+++ ClassObjectTask/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace ClassObjectTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //int[] arr = new int[] {};
+            //Console.WriteLine("5 eded daxil edin");
+
+            //for(int i = 0; i < 5; i++)
+            //{
+            //    string a = Console.ReadLine();
+            //    int b = int.Parse(a);
+            //    Array.Resize(ref arr, arr.Length + 1);
+            //    arr[arr.Length - 1] = b;
+            //}
+
+            //Console.WriteLine(CustomSort(arr));
+            #endregion
+            #region Task2
+            //string[] arr = {"World","Azerbaijan","Baku"};
+            //string symbol = Console.ReadLine();
+            //char ch = char.Parse(symbol);
+            //Console.WriteLine(FindChar(arr, ch));
+
+
+
+
+
+
+
+            #endregion
+        }
+
+            #region Task2
+        //static string [] FindChar(string [] arr , char ch)
+        //{
+
+        // Tamamlamamlanmayib hele.
+
+        //    for (int i = 0; i < arr.Length-1; i++)
+        //    {
+        //        for (int j = 0; j < arr[i].Length; j++)
+        //        {
+        //            char b = char.Parse(arr[j]);
+        //            if (ch==b)
+        //            {
+        //                Console.WriteLine(arr[j]);
+
+        //            }
+        //    }
+
+        //        }
+
+
+
+
+
+
+
+
+        //    return arr;
+
+
+
+
+        //}
+        #endregion
+            #region Task1
+        //static int [] CustomSort(int[] arr)
+        //{
+        //    int c;
+        //    for (int i = 0; i < arr.Length-1; i++)
+        //        for (int j = i + 1; j < arr.Length; j++)
+
+
+        //            if (arr[i] > arr[j])
+        //            {
+
+        //                c = arr[i];
+        //                arr[i] = arr[j];
+        //                arr[j] = c;
+        //            }
+        //    foreach (int item in arr)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //    return arr;
+
+        //}
+        #endregion
+    }
+
+}
+
+
+
+  
+
+
+
+
+
